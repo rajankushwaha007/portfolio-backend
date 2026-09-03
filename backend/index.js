902bb4ae0f251c81453dfbb0c4b0fffbeefe11f5
rajankushwaha <rajankushwaha7181@gmail.com>
2026-09-03 18:41:10 +0530

    Fix Vercel Express deployement

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,13 @@ const ContactRouter = require("./routes/contact.route");
 app.use("/api/contact", ContactRouter);
 
 app.get("/", (req, res) => {
-    res.send("Portfolio Backend is Running");
+  res.send("Portfolio Backend is Running");
 });
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is Running on port ${PORT}`);
+  console.log(`Server is Running on port ${PORT}`);
 });
 
 module.exports = app;
