70d95571a4fd2fc7c0fe38bbf7354a29e86e24d3
rajankushwaha <rajankushwaha7181@gmail.com>
2026-09-03 16:41:55 +0530

    Fix CORS for contact from

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
